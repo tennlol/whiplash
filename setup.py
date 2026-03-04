e819cdf9e23492bb139ee3e981ebd57dbc974e33
tenn <92182432+tennlol@users.noreply.github.com>
2026-03-04 21:12:02 +0000

    Add setup.py for whiplash package configuration

--- setup.py
+++ setup.py
@@ -1,1 +1,27 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="whiplash",
+    version="1.0.0",
+    author="tenn",
+    description="Host servers within seconds.",
+    long_description="Configuration. Servers. All yours -- in seconds.",
+    long_description_content_type="text/plain",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "watchdog",
+        "websockets",
+        "colorama"
+    ],
+    entry_points={
+        "console_scripts": [
+            "whiplash=whiplash.main:run"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
