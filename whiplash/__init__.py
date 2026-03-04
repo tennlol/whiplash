ef88ef4dee58d6de50b91f48070b8631499b4aa2
tenn <92182432+tennlol@users.noreply.github.com>
2026-03-04 21:13:24 +0000

    Add docstring and metadata to __init__.py
    Add module docstring and metadata variables

--- whiplash/__init__.py
+++ whiplash/__init__.py
@@ -1,1 +1,12 @@
+"""
+Whiplash
+Zero configuration lightning-fast local development server.
+"""
 
+from .main import run
+
+__version__ = "1.0.0"
+__author__ = "tenn"
+__license__ = "MIT"
+
+__all__ = ["run"]
